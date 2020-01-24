5c1a6f2780c27fba539aa515a4baa0a559a76c61
Praveen20K <58627337+Praveen20K@users.noreply.github.com>
2020-01-24 10:55:28 +0530

    Update TC_002FlipKart.java

--- MavenProject/src/main/java/testcase/TC_002FlipKart.java
+++ MavenProject/src/main/java/testcase/TC_002FlipKart.java
@@ -12,7 +12,6 @@ public class TC_002FlipKart extends FlipKart {
 		new homePage()
 		.visibilityOfImage()
 		.clickBanners()
-		.verifyFilter()
 		.priceLowToHigh()
 		.priceHighToLow()
 		.selectFromBanner()
